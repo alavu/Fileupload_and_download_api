0c4c90e4b4dc2db012ddf9533d6c174ad9031113
Alavudheen  E <alavudheen.e@claysys.com>
2022-10-12 19:59:19 +0530

    initails commit

---
+++ src/main/java/com/alavu/fileupload_and_download_api/FileuploadAndDownloadApiApplication.java
@@ -0,0 +1,13 @@
+package com.alavu.fileupload_and_download_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileuploadAndDownloadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileuploadAndDownloadApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alavu/fileupload_and_download_api/FileuploadAndDownloadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alavu.fileupload_and_download_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileuploadAndDownloadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
